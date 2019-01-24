f0a4c36b9fc87215d9f452213681f5a77bf655f5
Thomas Kolonko <tz.kolonko@gmail.com>
2019-01-24 18:04:58 +0100

    making FiveCrop visible and adding custom setup for it

--- template/runner/__init__.py
+++ template/runner/__init__.py
@@ -1,7 +1,8 @@
 from .apply_model import ApplyModel
 from .bidimensional import Bidimensional
 from .image_classification import ImageClassification
+from .image_classification_five_crop import ImageClassificationFiveCrop
 from .triplet import Triplet
 from .process_activation import ProcessActivation
 
-__all__ = ['ImageClassification', 'Bidimensional', 'Triplet', 'ApplyModel', 'ProcessActivation']
+__all__ = ['ImageClassification', 'ImageClassificationFiveCrop', 'Bidimensional', 'Triplet', 'ApplyModel', 'ProcessActivation']
