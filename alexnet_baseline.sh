b5842337d308d866a556cafb72be343dd192bc81
Thomas Kolonko <tz.kolonko@gmail.com>
2019-03-07 08:26:13 +0100

    Alexnet Baseline Script

--- alexnet_baseline.sh
+++ alexnet_baseline.sh
@@ -3,48 +3,48 @@
 # AlexNet --  --lr 0.1
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
-    --model-name alexnet --epochs 20 --experiment-name tz_asbestos_alexnet_1 \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
     --output-folder /home/thomas.kolonko/output_asbestos --ignoregit \
-    --lr 0.1 --optimizer-name Adam --decay-lr 5 --multi-run 5
+    --lr 0.1 --optimizer-name Adam --decay-lr 15 --multi-run 5
 
 # AlexNet --  --lr 0.05
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
-    --model-name alexnet --epochs 20 --experiment-name tz_asbestos_alexnet_2 \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
     --output-folder /home/thomas.kolonko/output_asbestos --ignoregit \
-    --lr 0.05 --optimizer-name Adam --decay-lr 5 --multi-run 5
+    --lr 0.05 --optimizer-name Adam --decay-lr 15 --multi-run 5
 
 # AlexNet --  --lr 0.01
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
-    --model-name alexnet --epochs 20 --experiment-name tz_asbestos_alexnet_3 \
-    --output-folder /home/thomas.kolonko/output_asbestos --ignoregit \
-    --lr 0.01 --optimizer-name Adam --decay-lr 5 --multi-run 5
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet --ignoregit \
+    --lr 0.01 --optimizer-name Adam --decay-lr 15 --multi-run 5
 
 # AlexNet --  --lr 0.005
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
-    --model-name alexnet --epochs 20 --experiment-name tz_asbestos_alexnet_4 \
-    --output-folder /home/thomas.kolonko/output_asbestos --ignoregit \
-    --lr 0.005 --optimizer-name Adam --decay-lr 5 --multi-run 5
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet --ignoregit \
+    --lr 0.005 --optimizer-name Adam --decay-lr 15 --multi-run 5
 
 # AlexNet --  --lr 0.001
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
-    --model-name alexnet --epochs 20 --experiment-name tz_asbestos_alexnet_5 \
-    --output-folder /home/thomas.kolonko/output_asbestos --ignoregit \
-    --lr 0.001 --optimizer-name Adam --decay-lr 5 --multi-run 5
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet --ignoregit \
+    --lr 0.001 --optimizer-name Adam --decay-lr 15 --multi-run 5
 
 # AlexNet --  --lr 0.0005
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
-    --model-name alexnet --epochs 20 --experiment-name tz_asbestos_alexnet_6 \
-    --output-folder /home/thomas.kolonko/output_asbestos --ignoregit \
-    --lr 0.0005 --optimizer-name Adam --decay-lr 5 --multi-run 5
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet --ignoregit \
+    --lr 0.0005 --optimizer-name Adam --decay-lr 15 --multi-run 5
 
 # AlexNet --  --lr 0.0001
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
-    --model-name alexnet --epochs 20 --experiment-name tz_asbestos_alexnet_7 \
-    --output-folder /home/thomas.kolonko/output_asbestos --ignoregit \
-    --lr 0.0001 --optimizer-name Adam --decay-lr 5 --multi-run 5
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet --ignoregit \
+    --lr 0.0001 --optimizer-name Adam --decay-lr 15 --multi-run 5
