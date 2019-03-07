71f0bd85625a818a38d0e25da7537e5c4aa1130b
Thomas Kolonko <tz.kolonko@gmail.com>
2019-03-07 08:31:21 +0100

    adding different architectures to repository

--- models/__init__.py
+++ models/__init__.py
@@ -4,13 +4,22 @@ from .FC_medium import FC_medium
 from .FC_simple import FC_simple
 from .TNet import TNet
 from .AlexNet import alexnet
-from .ResNet import resnet18, resnet34, resnet50, resnet101, resnet152
-from .ResNet18_Filter_One import resnet_one
-from .ResNet18_Filter_Two import resnet_two
-from .ResNet18_Filter_Four import resnet_four
-from .ResNet18_Filter_Eight import resnet_eigth
-from .ResNet18_Filter_Sixteen import resnet_sixteen
-from .ResNet18_Filter_ThirtyTwo import resnet_thirtytwo
+
+# DIFFERENT FILTERS IN ARCHITECTURE
+# **********************************
+from models.ResNet_Filters.ResNet18_Filter_One import resnet_one
+from models.ResNet_Filters.ResNet18_Filter_Two import resnet_two
+from models.ResNet_Filters.ResNet18_Filter_Four import resnet_four
+from models.ResNet_Filters.ResNet18_Filter_Eight import resnet_eigth
+from models.ResNet_Filters.ResNet18_Filter_Sixteen import resnet_sixteen
+from models.ResNet_Filters.ResNet18_Filter_ThirtyTwo import resnet_thirtytwo
+
+# DIFFERENT INPUT SIZES IN ARCHITECTURE
+# *************************************
+from models.ResNet_Image_Input.ResNet18_448 import resnet18_448
+from models.ResNet_Image_Input.ResNet18_896 import resnet18_896
+from models.ResNet_Image_Input.ResNet18_1024 import resnet18_1024
+
 from .BabyResNet import babyresnet18, babyresnet34, babyresnet50, babyresnet101, babyresnet152
 from ._VGG import vgg11, vgg11_bn, vgg13, vgg13_bn, vgg16, vgg16_bn, vgg19, vgg19_bn
 from ._Inception_v3 import inception_v3
