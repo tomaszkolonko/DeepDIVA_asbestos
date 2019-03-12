eb609491c41490e62c29677c9932e91e4c612e7a
Thomas Kolonko <tz.kolonko@gmail.com>
2019-03-12 15:26:01 +0100

    script updates for final runs

--- alexnet_baseline.sh
+++ alexnet_baseline.sh
@@ -2,49 +2,153 @@
 
 # AlexNet --  --lr 0.1
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
     --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
-    --output-folder /home/thomas.kolonko/output_asbestos --ignoregit \
-    --lr 0.1 --optimizer-name Adam --decay-lr 15 --multi-run 5
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_a --ignoregit \
+    --lr 0.1 --optimizer-name Adam --decay-lr 10
 
 # AlexNet --  --lr 0.05
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
     --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
-    --output-folder /home/thomas.kolonko/output_asbestos --ignoregit \
-    --lr 0.05 --optimizer-name Adam --decay-lr 15 --multi-run 5
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_a --ignoregit \
+    --lr 0.05 --optimizer-name Adam --decay-lr 10
 
 # AlexNet --  --lr 0.01
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
     --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
-    --output-folder /home/thomas.kolonko/output_asbestos_alexnet --ignoregit \
-    --lr 0.01 --optimizer-name Adam --decay-lr 15 --multi-run 5
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_a --ignoregit \
+    --lr 0.01 --optimizer-name Adam --decay-lr 10
 
 # AlexNet --  --lr 0.005
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
     --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
-    --output-folder /home/thomas.kolonko/output_asbestos_alexnet --ignoregit \
-    --lr 0.005 --optimizer-name Adam --decay-lr 15 --multi-run 5
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_a --ignoregit \
+    --lr 0.005 --optimizer-name Adam --decay-lr 10
 
 # AlexNet --  --lr 0.001
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
     --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
-    --output-folder /home/thomas.kolonko/output_asbestos_alexnet --ignoregit \
-    --lr 0.001 --optimizer-name Adam --decay-lr 15 --multi-run 5
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_a --ignoregit \
+    --lr 0.001 --optimizer-name Adam --decay-lr 10
 
 # AlexNet --  --lr 0.0005
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
     --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
-    --output-folder /home/thomas.kolonko/output_asbestos_alexnet --ignoregit \
-    --lr 0.0005 --optimizer-name Adam --decay-lr 15 --multi-run 5
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_a --ignoregit \
+    --lr 0.0005 --optimizer-name Adam --decay-lr 10
 
 # AlexNet --  --lr 0.0001
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
     --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
-    --output-folder /home/thomas.kolonko/output_asbestos_alexnet --ignoregit \
-    --lr 0.0001 --optimizer-name Adam --decay-lr 15 --multi-run 5
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_a --ignoregit \
+    --lr 0.0001 --optimizer-name Adam --decay-lr 10
+
+
+# ============================================================
+
+# AlexNet --  --lr 0.1
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aa --ignoregit \
+    --lr 0.1 --optimizer-name Adam --decay-lr 15
+
+# AlexNet --  --lr 0.05
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aa --ignoregit \
+    --lr 0.05 --optimizer-name Adam --decay-lr 15
+
+# AlexNet --  --lr 0.01
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aa --ignoregit \
+    --lr 0.01 --optimizer-name Adam --decay-lr 15
+
+# AlexNet --  --lr 0.005
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aa --ignoregit \
+    --lr 0.005 --optimizer-name Adam --decay-lr 15
+
+# AlexNet --  --lr 0.001
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aa --ignoregit \
+    --lr 0.001 --optimizer-name Adam --decay-lr 15
+
+# AlexNet --  --lr 0.0005
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aa --ignoregit \
+    --lr 0.0005 --optimizer-name Adam --decay-lr 15
+
+# AlexNet --  --lr 0.0001
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aa --ignoregit \
+    --lr 0.0001 --optimizer-name Adam --decay-lr 15
+
+
+# ==========================================================================================
+
+# AlexNet --  --lr 0.1
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aaa --ignoregit \
+    --lr 0.1 --optimizer-name Adam --decay-lr 20
+
+# AlexNet --  --lr 0.05
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aaa --ignoregit \
+    --lr 0.05 --optimizer-name Adam --decay-lr 20
+
+# AlexNet --  --lr 0.01
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aaa --ignoregit \
+    --lr 0.01 --optimizer-name Adam --decay-lr 20
+
+# AlexNet --  --lr 0.005
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aaa --ignoregit \
+    --lr 0.005 --optimizer-name Adam --decay-lr 20
+
+# AlexNet --  --lr 0.001
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aaa --ignoregit \
+    --lr 0.001 --optimizer-name Adam --decay-lr 20
+
+# AlexNet --  --lr 0.0005
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aaa --ignoregit \
+    --lr 0.0005 --optimizer-name Adam --decay-lr 20
+
+# AlexNet --  --lr 0.0001
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_aaa --ignoregit \
+    --lr 0.0001 --optimizer-name Adam --decay-lr 20
