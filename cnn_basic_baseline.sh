a1bfe52dbf57e4b3f72937dcf3409e91b0e070b0
Thomas Kolonko <tz.kolonko@gmail.com>
2019-03-12 19:09:15 +0100

    script for CNN_basis baseline

---
+++ cnn_basic_baseline.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+# CNN_Basic --  --lr 0.1
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_a --ignoregit \
+    --lr 0.1 --optimizer-name Adam --decay-lr 10
+
+# CNN_Basic --  --lr 0.05
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_a --ignoregit \
+    --lr 0.05 --optimizer-name Adam --decay-lr 10
+
+# CNN_Basic --  --lr 0.01
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_a --ignoregit \
+    --lr 0.01 --optimizer-name Adam --decay-lr 10
+
+# CNN_Basic --  --lr 0.005
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_a --ignoregit \
+    --lr 0.005 --optimizer-name Adam --decay-lr 10
+
+# CNN_Basic --  --lr 0.001
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_a --ignoregit \
+    --lr 0.001 --optimizer-name Adam --decay-lr 10
+
+# CNN_Basic --  --lr 0.0005
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_a --ignoregit \
+    --lr 0.0005 --optimizer-name Adam --decay-lr 10
+
+# CNN_Basic --  --lr 0.0001
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_a --ignoregit \
+    --lr 0.0001 --optimizer-name Adam --decay-lr 10
+
+
+# ============================================================
+
+# CNN_Basic --  --lr 0.1
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aa --ignoregit \
+    --lr 0.1 --optimizer-name Adam --decay-lr 15
+
+# CNN_Basic --  --lr 0.05
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aa --ignoregit \
+    --lr 0.05 --optimizer-name Adam --decay-lr 15
+
+# CNN_Basic --  --lr 0.01
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aa --ignoregit \
+    --lr 0.01 --optimizer-name Adam --decay-lr 15
+
+# CNN_Basic --  --lr 0.005
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aa --ignoregit \
+    --lr 0.005 --optimizer-name Adam --decay-lr 15
+
+# CNN_Basic --  --lr 0.001
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aa --ignoregit \
+    --lr 0.001 --optimizer-name Adam --decay-lr 15
+
+# CNN_Basic --  --lr 0.0005
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aa --ignoregit \
+    --lr 0.0005 --optimizer-name Adam --decay-lr 15
+
+# CNN_Basic --  --lr 0.0001
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aa --ignoregit \
+    --lr 0.0001 --optimizer-name Adam --decay-lr 15
+
+
+# ==========================================================================================
+
+# CNN_Basic --  --lr 0.1
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aaa --ignoregit \
+    --lr 0.1 --optimizer-name Adam --decay-lr 20
+
+# CNN_Basic --  --lr 0.05
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aaa --ignoregit \
+    --lr 0.05 --optimizer-name Adam --decay-lr 20
+
+# CNN_Basic --  --lr 0.01
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aaa --ignoregit \
+    --lr 0.01 --optimizer-name Adam --decay-lr 20
+
+# CNN_Basic --  --lr 0.005
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aaa --ignoregit \
+    --lr 0.005 --optimizer-name Adam --decay-lr 20
+
+# CNN_Basic --  --lr 0.001
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aaa --ignoregit \
+    --lr 0.001 --optimizer-name Adam --decay-lr 20
+
+# CNN_Basic --  --lr 0.0005
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aaa --ignoregit \
+    --lr 0.0005 --optimizer-name Adam --decay-lr 20
+
+# CNN_Basic --  --lr 0.0001
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name CNN_basic --epochs 50 --experiment-name tz_cnn_basic_alexnet \
+    --output-folder /home/thomas.kolonko/output_asbestos_cnn_basic_aaa --ignoregit \
+    --lr 0.0001 --optimizer-name Adam --decay-lr 20
