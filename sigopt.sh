40129277bb27fd58dc697a2bf3c87802d319f0bb
Thomas Kolonko <tz.kolonko@gmail.com>
2019-03-13 23:22:41 +0100

    extended sigopt script

--- sigopt.sh
+++ sigopt.sh
@@ -31,3 +31,35 @@ python ./template/RunMe.py --runner-class image_classification \
     --output-folder /home/thomas.kolonko/output_asbestos_resnet18_sigopt --ignoregit --lr 0.01 \
     --momentum 0.9 --decay-lr 20 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
     --sig-opt-runs 30 --sig-opt util/sigopt.json
+
+# Resnet34 ----------------------------------------
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name resnet34 --epochs 50 --experiment-name tz_asbestos_resnet34_sigopt \
+    --output-folder /home/thomas.kolonko/output_asbestos_resnet34_sigopt --ignoregit --lr 0.01 \
+    --momentum 0.9 --decay-lr 20 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
+    --sig-opt-runs 30 --sig-opt util/sigopt.json
+
+# Resnet34 PRE-TRAINED ----------------------------------------
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name resnet34 --epochs 50 --experiment-name tz_asbestos_resnet34_sigopt_pre \
+    --output-folder /home/thomas.kolonko/output_asbestos_resnet34_sigopt --ignoregit --lr 0.01 \
+    --momentum 0.9 --decay-lr 20 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
+    --sig-opt-runs 30 --sig-opt util/sigopt.json
+
+# densenet121 ----------------------------------------
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name densenet121 --epochs 50 --experiment-name tz_asbestos_densenet121_sigopt \
+    --output-folder /home/thomas.kolonko/output_asbestos_densenet121_sigopt --ignoregit --lr 0.01 \
+    --momentum 0.9 --decay-lr 20 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
+    --sig-opt-runs 30 --sig-opt util/sigopt.json
+
+# densenet121 PRE-TRAINED ----------------------------------------
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name densenet121 --epochs 50 --experiment-name tz_asbestos_densenet121_sigopt_pre \
+    --output-folder /home/thomas.kolonko/output_asbestos_densenet121_sigopt --ignoregit --lr 0.01 \
+    --momentum 0.9 --decay-lr 20 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
+    --sig-opt-runs 30 --sig-opt util/sigopt.json
