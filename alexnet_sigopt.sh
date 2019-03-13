f54d0b2b1019005d040e3a769e33d1050dcde560
Thomas Kolonko <tz.kolonko@gmail.com>
2019-03-13 19:30:50 +0100

    additional script

--- alexnet_sigopt.sh
+++ alexnet_sigopt.sh
@@ -2,8 +2,16 @@
 
 # AlexNet ----------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
-    --dataset-folder /home/thomas.kolonko/ASBESTOS/ \
-    --model-name alexnet --epochs 20 --experiment-name tz_asbestos_alexnet \
-    --output-folder /home/thomas.kolonko/output_asbestos --ignoregit --lr 0.01 \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet_sigopt \
+    --output-folder /home/thomas.kolonko/output_asbestos_sigopt --ignoregit --lr 0.01 \
+    --momentum 0.9 --decay-lr 5 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
+    --sig-opt-runs 30 --sig-opt util/sigopt.json
+
+# AlexNet ----------------------------------------
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name resnet18 --epochs 50 --experiment-name tz_asbestos_alexnet_sigopt \
+    --output-folder /home/thomas.kolonko/output_asbestos_sigopt --ignoregit --lr 0.01 \
     --momentum 0.9 --decay-lr 5 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
     --sig-opt-runs 30 --sig-opt util/sigopt.json
--- sigopt.sh
+++ sigopt.sh
@@ -4,14 +4,30 @@
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/FINAL/ \
     --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet_sigopt \
-    --output-folder /home/thomas.kolonko/output_asbestos_sigopt --ignoregit --lr 0.01 \
-    --momentum 0.9 --decay-lr 5 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_sigopt --ignoregit --lr 0.01 \
+    --momentum 0.9 --decay-lr 20 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
     --sig-opt-runs 30 --sig-opt util/sigopt.json
 
-# AlexNet ----------------------------------------
+# AlexNet PRE-TRAINED ----------------------------------------
 python ./template/RunMe.py --runner-class image_classification \
     --dataset-folder /home/thomas.kolonko/FINAL/ \
-    --model-name resnet18 --epochs 50 --experiment-name tz_asbestos_alexnet_sigopt \
-    --output-folder /home/thomas.kolonko/output_asbestos_sigopt --ignoregit --lr 0.01 \
-    --momentum 0.9 --decay-lr 5 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
+    --model-name alexnet --epochs 50 --experiment-name tz_asbestos_alexnet_sigopt_pre \
+    --output-folder /home/thomas.kolonko/output_asbestos_alexnet_sigopt --ignoregit --lr 0.01 \
+    --momentum 0.9 --decay-lr 20 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
+    --sig-opt-runs 30 --sig-opt util/sigopt.json --pretrained
+
+# Resnet18 ----------------------------------------
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name resnet18 --epochs 50 --experiment-name tz_asbestos_resnet18_sigopt \
+    --output-folder /home/thomas.kolonko/output_asbestos_resnet18_sigopt --ignoregit --lr 0.01 \
+    --momentum 0.9 --decay-lr 20 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
+    --sig-opt-runs 30 --sig-opt util/sigopt.json
+
+# Resnet18 PRE-TRAINED ----------------------------------------
+python ./template/RunMe.py --runner-class image_classification \
+    --dataset-folder /home/thomas.kolonko/FINAL/ \
+    --model-name resnet18 --epochs 50 --experiment-name tz_asbestos_resnet18_sigopt_pre \
+    --output-folder /home/thomas.kolonko/output_asbestos_resnet18_sigopt --ignoregit --lr 0.01 \
+    --momentum 0.9 --decay-lr 20 --sig-opt-token CEUJGDONOZMDYVYJLRWBJUGTQZKCLTFFPFGGKQDUDBZDHZCL \
     --sig-opt-runs 30 --sig-opt util/sigopt.json
