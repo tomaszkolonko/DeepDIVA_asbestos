fixed font scaling on confusion matrix

util/visualization/cumstom_confusion_matrix.py
@@ -30,21 +30,25 @@ def make_heatmap(confusion_matrix, class_names):
         confusion_matrix, index=class_names, columns=class_names,
     )
 
+
     plt.style.use(['seaborn-white', 'seaborn-paper'])
+    sns.set(font_scale=2.5)
+
     fig = plt.figure(figsize=(14, 14))
+    ax1 = fig.add_axes([0.3, 0.05, 0.7, 0.7])
     plt.tight_layout()
 
-    heatmap = sns.heatmap(df_cm, annot=True, fmt="d", cmap=plt.get_cmap('Blues'), annot_kws={"size": 14})
+    heatmap = sns.heatmap(df_cm, ax=ax1, annot=True, fmt="d", cmap=plt.get_cmap('Blues'), annot_kws={"size": 32})
 
-    heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=14)
-    heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=45, ha='right', fontsize=14)
+    heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=28)
+    heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=45, ha='center', fontsize=28)
     heatmap.xaxis.set_ticks_position('top')
     heatmap.xaxis.set_label_position('top')
 
-    xlabel_t = plt.xlabel("PREDICTED CLASS", fontsize=18)
+    xlabel_t = plt.xlabel("PREDICTED CLASS", fontsize=32)
     xlabel_t.set_position([0.5, 12])
 
-    plt.ylabel("ACTUAL CLASS", fontsize=18)
+    plt.ylabel("ACTUAL CLASS", fontsize=32)
 
     fig.canvas.draw()
     data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')